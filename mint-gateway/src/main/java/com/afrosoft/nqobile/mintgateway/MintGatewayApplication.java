9473ee69a72b9ed0e76e694d9e778b90481698e1
given <given@ziqni.com>
2026-07-17 13:54:37 +0200

    Added mint-gateway

---
+++ mint-gateway/src/main/java/com/afrosoft/nqobile/mintgateway/MintGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.afrosoft.nqobile.mintgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintGatewayApplication.class, args);
+    }
+
+}
