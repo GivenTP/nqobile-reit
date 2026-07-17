c8596a6c0c408172ef5a9b24a12185c6f617bc08
given <given@ziqni.com>
2026-07-17 13:40:06 +0200

    Added complaints service

---
+++ spv-admin-service/src/main/java/com/afrosoft/nqobile/spvadminservice/SpvAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.afrosoft.nqobile.spvadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpvAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpvAdminServiceApplication.class, args);
+    }
+
+}
---
+++ spv-admin-service/src/test/java/com/afrosoft/nqobile/spvadminservice/SpvAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrosoft.nqobile.spvadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpvAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
