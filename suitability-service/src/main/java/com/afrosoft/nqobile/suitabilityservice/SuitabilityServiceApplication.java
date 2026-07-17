7438960f9b8866674f31974226cc50ea67922617
given <given@ziqni.com>
2026-07-17 13:11:44 +0200

    Added suitability-service

---
+++ suitability-service/src/main/java/com/afrosoft/nqobile/suitabilityservice/SuitabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.afrosoft.nqobile.suitabilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuitabilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuitabilityServiceApplication.class, args);
+    }
+
+}
---
+++ suitability-service/src/test/java/com/afrosoft/nqobile/suitabilityservice/SuitabilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrosoft.nqobile.suitabilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuitabilityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
